013e370cd835ec96696e3e7167598ce7198af13c
Jimmy <jyng02@gmail.com>
2018-06-19 21:24:01 -0700

    getting sound into an array

--- sound.js
+++ sound.js
@@ -1,14 +1,55 @@
-var sound = new Howl({
-  src: ['sound/getting-off-bus.mp3', 'sound/higgs/mp3'],
-});
-// Play returns a unique Sound ID that can be passed
-// into any method on Howl to control that specific sound.
-var id1 = sound.play();
-var id2 = sound.play();
+// stage1= [];
+//
+//
+// var id1 = new Howl({
+//   src: ['sound/getting-off-bus.mp3'],
+// });
+//
+// var id2 = new Howl({
+//   src: ['sound/higg.mp3'],
+// });
+//
+// var id3 = new Howl({
+//   src: ['sound/a.mp3'],
+// });
+//
+// var id4 = new Howl({
+//   src: ['sound/b.mp3'],
+// });
+//
+// var id5 = new Howl({
+//   src: ['sound/c.mp3'],
+// });
+//
+// var id6 = new Howl({
+//   src: ['sound/d.mp3'],
+// });
 
-// Fade out the first sound and speed up the second.
-sound.fade(1, 0, 10000, id1);
-sound.fade(0,1, 10000, id2);
+window.onload = function(e) {
+    // initialisation:
+    var onPlay = [false],  // this one is useless now
+      pCount = 0;
+      playlistUrls = [
+        "/sound/a.mp3",
+        "/sound/b.mp3",
+        "/sound/c.mp3",
+        "/sound/home.mp3"
+        ], // audio list
+      howlerBank = []
+      loop = true;
 
-console.log(id1);
-console.log(id2);
+    // playing i+1 audio (= chaining audio files)
+    var onEnd = function(e) {
+      if (loop === true ) { pCount = (pCount + 1 !== howlerBank.length)? pCount + 1 : 0; }
+      else { pCount = pCount + 1; }
+      howlerBank[pCount].play();
+    };
+
+    // build up howlerBank:
+    playlistUrls.forEach(function(current, i) {
+      howlerBank.push(new Howl({ src: [playlistUrls[i]], onend: onEnd, buffer: true }))
+    });
+
+    // initiate the whole :
+        howlerBank[0].play();
+}
