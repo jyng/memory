403a470ffe9688a3995d1332fcd4d411e4065aa2
Jimmy <jyng02@gmail.com>
2018-06-19 23:42:31 -0700

    setting up crossfade

--- sound.js
+++ sound.js
@@ -1,42 +1,16 @@
-// stage1= [];
-//
-//
-// var id1 = new Howl({
-//   src: ['sound/getting-off-bus.mp3'],
-// });
-//
-// var id2 = new Howl({
-//   src: ['sound/higg.mp3'],
-// });
-//
-// var id3 = new Howl({
-//   src: ['sound/a.mp3'],
-// });
-//
-// var id4 = new Howl({
-//   src: ['sound/b.mp3'],
-// });
-//
-// var id5 = new Howl({
-//   src: ['sound/c.mp3'],
-// });
-//
-// var id6 = new Howl({
-//   src: ['sound/d.mp3'],
-// });
 
 window.onload = function(e) {
     // initialisation:
     var onPlay = [false],  // this one is useless now
       pCount = 0;
       playlistUrls = [
-        "/sound/a.mp3",
-        "/sound/b.mp3",
-        "/sound/c.mp3",
-        "/sound/home.mp3"
+        "/sound/4dd.ogg",
+        "/sound/2p.mp3",
+        "/sound/3m.mp3"
+
         ], // audio list
       howlerBank = []
-      loop = true;
+      loop = false;
 
     // playing i+1 audio (= chaining audio files)
     var onEnd = function(e) {
@@ -47,8 +21,71 @@ window.onload = function(e) {
 
     // build up howlerBank:
     playlistUrls.forEach(function(current, i) {
-      howlerBank.push(new Howl({ src: [playlistUrls[i]], onend: onEnd, buffer: true }))
+      howlerBank.push(new Howl({
+        src: [playlistUrls[i]],
+        onend: onEnd,
+        buffer: true }))
     });
+
+    var CrossfadeSample = {playing:false};
+
+    CrossfadeSample.play = function() {
+      // Create two sources.
+      this.ctl1 = createSource(BUFFERS.drums);
+      this.ctl2 = createSource(BUFFERS.organ);
+      // Mute the second source.
+      this.ctl1.gainNode.gain.value = 0;
+      // Start playback in a loop
+      if (!this.ctl1.source.start) {
+        this.ctl1.source.noteOn(0);
+        this.ctl2.source.noteOn(0);
+      } else {
+        this.ctl1.source.start(0);
+        this.ctl2.source.start(0);
+      }
+
+      function createSource(buffer) {
+        var source = context.createBufferSource();
+        var gainNode = context.createGain ? context.createGain() : context.createGainNode();
+        source.buffer = buffer;
+        // Turn on looping
+        source.loop = true;
+        // Connect source to gain.
+        source.connect(gainNode);
+        // Connect gain to destination.
+        gainNode.connect(context.destination);
+
+        return {
+          source: source,
+          gainNode: gainNode
+        };
+      }
+    };
+
+    CrossfadeSample.stop = function() {
+      if (!this.ctl1.source.stop) {
+        this.ctl1.source.noteOff(0);
+        this.ctl2.source.noteOff(0);
+      } else {
+        this.ctl1.source.stop(0);
+        this.ctl2.source.stop(0);
+      }
+    };
+
+    // Fades between 0 (all source 1) and 1 (all source 2)
+    CrossfadeSample.crossfade = function(element) {
+      var x = parseInt(element.value) / parseInt(element.max);
+      // Use an equal-power crossfading curve:
+      var gain1 = Math.cos(x * 0.5*Math.PI);
+      var gain2 = Math.cos((1.0 - x) * 0.5*Math.PI);
+      this.ctl1.gainNode.gain.value = gain1;
+      this.ctl2.gainNode.gain.value = gain2;
+    };
+
+    CrossfadeSample.toggle = function() {
+      this.playing ? this.stop() : this.play();
+      this.playing = !this.playing;
+    };
 
     // initiate the whole :
         howlerBank[0].play();
